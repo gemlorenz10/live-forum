ebad7d097909eda3a8c963b9590ee7fe13066f48
Gem Anonuevo <gemlorenz10@gmail.com>
2018-04-03 23:44:15 +0800

    Update setting up environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RegisterPage } from './pages/register-page/register-page';
+import { ForumPage } from './pages/forum-page/forum-page';
+import { LoginPage } from './pages/login-page/login-page';
+
+
+const appRoutes: Routes = [
+  { path: 'register', component: RegisterPage },
+  { path: 'login', component: LoginPage },
+  { path: '', component: ForumPage }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPage,
+    ForumPage,
+    LoginPage
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/forum-page/forum-page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'forum-page',
+  templateUrl: './forum-page.html',
+  styleUrls: ['./forum-page.scss']
+})
+export class ForumPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
